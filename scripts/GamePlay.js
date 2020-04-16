8493ded6c45655a13eeb4c9460e23732cd4e03aa
Stefan Ghioci <ghioci.stefan@gmail.com>
2020-04-16 15:57:15 +0300

    Add player movement

--- scripts/GamePlay.js
+++ scripts/GamePlay.js
@@ -20,10 +20,12 @@ var gamePlayState = new Phaser.Class({
 
     this.selectedCharacterString = state.selectedCharacter === 0 ? 'pikachu' : 'eevee';
 
-    this.player = this.add.sprite(60, 240, this.selectedCharacterString + '_idle').setScale(2);
+    this.player = this.physics.add.sprite(60, 240, this.selectedCharacterString + '_idle').setScale(2);
 
-    this.player.anims.play(this.selectedCharacterString + '_run');
-
+    this.player.anims.play(this.selectedCharacterString + '_idle');
+    this.player.anims.pause();
+    
+    
     this.cursors = this.input.keyboard.createCursorKeys();
 
     this.myCam = this.cameras.main;
@@ -33,14 +35,28 @@ var gamePlayState = new Phaser.Class({
   },
 
   update: function () {
-    this.player.x += 3;
+    this.player.setVelocity(0);
 
-    if (this.cursors.up.isDown && this.player.y > 240 - 32) {
-      this.player.y -= 2;
-    } else if (this.cursors.down.isDown && this.player.y < 480 - 64 - 64) {
-      this.player.y += 2;
+    if (this.cursors.left.isDown && this.player.x > 32) {
+      this.player.setVelocityX(-250);
+      this.player.flipX = true;
+      this.player.anims.play(this.selectedCharacterString + '_run', true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(250);
+      this.player.flipX = false;
+      this.player.anims.play(this.selectedCharacterString + '_run', true);
     }
 
+    if (this.cursors.up.isDown && this.player.y > 210) {
+      this.player.setVelocityY(-150);
+      this.player.anims.play(this.selectedCharacterString + '_run', true);
+    } else if (this.cursors.down.isDown &&this.player.y < 360) {
+      this.player.setVelocityY(150);
+      this.player.anims.play(this.selectedCharacterString + '_run', true);
+    } else if(!this.cursors.left.isDown && !this.cursors.right.isDown) {
+      this.player.anims.play(this.selectedCharacterString + '_idle', true);
+      this.player.anims.pause();
+    }
     this.topBackground.tilePositionX = this.myCam.scrollX * 0.75;
     this.bottomBackground.tilePositionX = this.myCam.scrollX * 1.25;
   },
